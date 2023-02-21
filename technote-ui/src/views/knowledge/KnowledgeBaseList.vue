da3cf1d838b3d4bea3e4ce43c544b304d0ca1a41
Leon <mail@omgxy.com>
2023-02-21 23:54:29 +0800

    chore: (feat) 知识库列表样式调整

--- technote-ui/src/views/knowledge/KnowledgeBaseList.vue
+++ technote-ui/src/views/knowledge/KnowledgeBaseList.vue
@@ -1,17 +1,17 @@
 <template>
   <el-row class="mb-4">
-    
-      <el-button type="primary" @click="kbNew">
-        <el-icon>
-          <Plus />
-        </el-icon>新建知识库
-      </el-button>
-
+    <el-button type="primary" @click="kbNew">
+      <el-icon>
+        <Plus />
+      </el-icon>
+      新建知识库
+    </el-button>
     <router-link to='/knowledgeCategory'>
       <el-button type="primary">
         <el-icon>
           <Collection />
-        </el-icon>分类管理
+        </el-icon>
+        分类管理
       </el-button>
     </router-link>
   </el-row>
@@ -20,29 +20,27 @@
   <el-row>
     <el-radio-group v-model="category" size="large">
       <el-radio-button label="全部" @click="categorySwitch(null)" />
-      <el-radio-button v-for="item in categoryList" :key="item.id" :label="item.name"
-        @click="categorySwitch(item.id)" />
+      <el-radio-button v-for="item in categoryList" :key="item.id" :label="item.name" @click="categorySwitch(item.id)" />
     </el-radio-group>
   </el-row>
-  <div
-    style="display: flex; flex-wrap: wrap; flex-grow: 1; justify-content: left">
-
+  <div style="display: flex; flex-wrap: wrap; flex-grow: 1; justify-content: left">
     <el-card :body-style="{ padding: '0px' }" v-for="(item, index) in knowledgeBaseList" :key="item.id">
-      <img :src="item.coverImage" class="image" @click="enterKb(item.id)"/>
-      <div style="padding: 14px">
-        <span>{{ item.name }}</span>
-        <div class="bottom">
-
-          <el-button text class="button">{{ item.description }}</el-button>
-          <el-button :icon="Edit" size="large" @click="editPageMeta(item.id)" circle />
+      <div>
+        <div class="image">
+          <img :src="item.coverImage" @click="enterKb(item.id)" />
+          <div class="card-edit">
+            <el-icon>
+              <Edit @click="editPageMeta(item.id)" />
+            </el-icon>
+          </div>
+        </div>
+        <div class="book-name">
+          <span>{{ item.name }}</span>
         </div>
       </div>
     </el-card>
   </div>
-
-  <el-empty style="margin-top: 200px" description="暂无数据" v-if="knowledgeBaseList.length == 0"/>
-
-
+  <el-empty style="margin-top: 200px" description="暂无数据" v-if="knowledgeBaseList.length == 0" />
 </template>
 
 <script lang="ts" setup>
@@ -50,10 +48,6 @@ import api from '../../api'
 import { ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 const currentDate = ref(new Date())
-
-import {
-  Edit
-} from '@element-plus/icons-vue'
 
 interface Kb {
   id: string,
@@ -88,11 +82,11 @@ api.post('/book/list', {
 const lock = ref(false)
 
 const enterKb = (id) => {
-  router.push({'path': '/knowledgeDetail', 'query': {'id': id}});
+  router.push({ 'path': '/knowledgeDetail', 'query': { 'id': id } });
 }
 
 const kbNew = () => {
-  router.push({'path': '/knowledgeNew', 'query': {'category': categoryId.value}});
+  router.push({ 'path': '/knowledgeNew', 'query': { 'category': categoryId.value } });
 }
 
 const categorySwitch = (id) => {
@@ -122,51 +116,50 @@ api.post('/category/list', {}).then(response => {
 });
 
 const editPageMeta = (id) => {
-  router.push({'path': '/knowledgeNew', 'query': {'id': id}});
+  router.push({ 'path': '/knowledgeNew', 'query': { 'id': id } });
 }
 
 </script>
 
 <style>
-.time {
-  font-size: 12px;
-  color: #999;
-}
-
-.bottom {
-  margin-top: 13px;
-  line-height: 12px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.button {
-  padding: 0;
-  min-height: auto;
-}
-
 .el-card {
-
   margin-top: 30px;
   margin-bottom: 10px;
-
   margin-right: 30px;
-  border: 4px solid #EFEFF4 !important;
-  border-radius: 10px !important;
+  border: 2px solid #35363A !important;
+  border-radius: 6px !important;
   cursor: pointer;
 }
 
 .el-card:hover {
-  border: 4px solid yellow !important;
+  border: 2px solid #41B883 !important;
 }
 
 .image {
-  width: 200px;
-  height: 190px;
-  border-bottom: 4px solid #EFEFF4;
-  padding: 20px 50px;
+  display: flex;
+  border-bottom: 1px dashed #35363A;
+}
 
+.image>img {
+  width: 40px;
+  height: 40px;
+  padding: 20px 50px;
+  padding-left: 60px;
+  margin-left: 20px;
+}
+
+.card-edit {
+  font-size: 20px;
+  color: #35363A;
+  margin: 10px;
+}
+
+.card-edit:hover {
+  color: #41B883;
+}
+
+.book-name {
+  margin: 8px 16px;
 }
 
 .el-row {
